5c3794b250778e651ebb7de88b596611de68daa9
Wildson Lab <wildson.lab@gmail.com>
2022-08-02 20:29:45 -0300

    Update "teste.py"

--- teste.py
+++ teste.py
@@ -2,5 +2,21 @@ def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
+def deposita(conta, valor):
+    conta["saldo"] += valor
 
-print(cria_conta(123, "Wildson", 1200.00, 3600.00))
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
+
+
+conta = cria_conta(123, "Wildson", 1200.00, 3600.00)
+extrato(conta)
+deposita(conta, 1500.00)
+saca(conta, 300)
+extrato(conta)
+

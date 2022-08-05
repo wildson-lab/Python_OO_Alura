33b6b14ea7b85f45340b54667b5141b0cb0af3e3
Wildson Lab <wildson.lab@gmail.com>
2022-08-04 21:34:37 -0300

    Getters e Setters

--- conta.py
+++ conta.py
@@ -20,3 +20,14 @@ class Conta:
         self.saca(valor)
         destino.deposita(valor)
 
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from conta import Conta
 
 conta1 = Conta(123, "Nico", 55.5, 1000.0)
-conta2 = Conta(321, "Marco", 100.0, 1000.0)
 
-# Agora essa parte não está mais acessível, pois os atributos foram encapsulados.
-# print(conta1.titular)
-# print(conta1.saldo)
-
-conta1.extrato()
-conta2.extrato()
-conta1.transfere(10.0,conta2)
-conta1.extrato()
-conta2.extrato()
+print(conta1.get_titular())
+print(conta1.get_saldo())
+print(conta1.get_limite())
+conta1.set_limite(10000.0)
+print(conta1.get_limite())

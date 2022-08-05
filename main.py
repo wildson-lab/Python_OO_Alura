748845a7911a89781bb668339572fd370f3fd32f
Wildson Lab <wildson.lab@gmail.com>
2022-08-04 21:09:33 -0300

    Encapsulamento de atributos e coesão

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from conta import Conta
 
-conta = Conta(123, "Nico", 55.5, 1000.0)
+conta1 = Conta(123, "Nico", 55.5, 1000.0)
+conta2 = Conta(321, "Marco", 100.0, 1000.0)
 
-print(conta.titular)
-print(conta.saldo)
+# Agora essa parte não está mais acessível, pois os atributos foram encapsulados.
+# print(conta1.titular)
+# print(conta1.saldo)
 
-conta.extrato()
-conta.deposita(15.0)
-conta.extrato()
-conta.saca(10.0)
-conta.extrato()
+conta1.extrato()
+conta2.extrato()
+conta1.transfere(10.0,conta2)
+conta1.extrato()
+conta2.extrato()

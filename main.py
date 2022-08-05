7845be389d63e5facddee071da1afe04719adb80
Wildson Lab <wildson.lab@gmail.com>
2022-08-04 21:44:09 -0300

    Utilizando propriedades

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from conta import Conta
 
 conta1 = Conta(123, "Nico", 55.5, 1000.0)
 
-print(conta1.get_titular())
-print(conta1.get_saldo())
-print(conta1.get_limite())
-conta1.set_limite(10000.0)
-print(conta1.get_limite())
+print(conta1.titular)
+print(conta1.saldo)
+print(conta1.limite)
+conta1.limite = 10000.0
+print(conta1.limite)
